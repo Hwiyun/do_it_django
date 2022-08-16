3046dde8f628c81fb18887208559940e1501b809
Hwiyun Jeong <Hwiyun>
2022-08-16 10:44:48 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,13 +1,14 @@
-from django.test import TestCase
-
-# Create your tests here.
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
-
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump',
+                                                   password='1q2w3e4r!')
+        self.user_obama = User.objects.create_user(username='obama',
+                                                   password='1q2w3e4r!')
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -15,35 +16,39 @@ class TestView(TestCase):
         self.assertIn('About Me', navbar.text)
 
         logo_btn = navbar.find('a', text='스마트 부산')
-        self.assertEqual(logo_btn.attrs['href'],'/')
+        self.assertEqual(logo_btn.attrs['href'], '/')
 
-        home_btn = navbar.find('a', text='home')
+        home_btn = navbar.find('a', text='Home')
         self.assertEqual(home_btn.attrs['href'], '/')
 
         blog_btn = navbar.find('a', text='Blog')
         self.assertEqual(blog_btn.attrs['href'], '/blog/')
 
         about_me_btn = navbar.find('a', text='About Me')
-        self.assertEqual(about_me_btn.attrs['href'], '/about me/')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        self.navber_test(soup)
+
+        self.navbar_test(soup)
 
         self.assertEqual(Post.objects.count(), 0)
+
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
         post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.',
-            content='Hello World. We are the world.',
+            title='첫 번쩨 포스트입니다,',
+            content='Hello Word. We are the world.',
+            author=self.user_trump
         )
         post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
-            content='1등이 전부는 아니잖아요?',
+            content='2등이 전부는 아니잖아요?',
+            author=self.user_obama
         )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -51,15 +56,20 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(response.status_code, 200)
 
-        main_area = soup.find('div', id='main-area')
+        main_area=soup.find('div', id='main-area')
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
+
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+        self.assertIn(post_001.author.username.upper(), main_area.text)
+        self.assertIn(post_002.author.username.upper(), main_area.text)
+
 
     def test_post_detail(self):
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
-            content='Hello World. We are the world.',
+            content='Hello Wold. We are the world.',
         )
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
@@ -70,9 +80,10 @@ class TestView(TestCase):
         self.navbar_test(soup)
 
         self.assertIn(post_001.title, soup.title.text)
-
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
         self.assertIn(post_001.title, post_area.text)
 
-        self.assertIn(post_001.content, post_area.text)
+
+
+# Create your tests here.
